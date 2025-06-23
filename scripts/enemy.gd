3b06b233ce50b70b6e500f97b5743a4683a83c50
Jaime <jaimemartinezrumoroso@gmail.com>
2025-06-23 16:45:56 -0400

    Fixed enemy collision and added walls

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,10 @@
 extends StaticBody2D
 
+const tile_size: Vector2=Vector2(16,16)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	queue_free()
+
+func _on_timer_timeout() -> void:
+	if !$Up.is_colliding():
+		global_position -= Vector2(0,1) * tile_size
